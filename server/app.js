3cb39ca03ddb5e2ba6da70c28f0a64f5a4afb80b
anthonycheitzeberg <a.heitzeberg007@gmail.com>
2021-09-14 15:47:19 +0800

    add graphql and nodemon packages

--- server/app.js
+++ server/app.js
@@ -1,8 +1,22 @@
 const express = require('express');
+/**
+ * Old school way: const graphqlHTTP = require('express-graphql');
+ * New ways:
+ *  (a)  const graphqlHTTP = require('express-graphql').graphqlHTTP; 
+ *  (b)  const { graphqlHTTP } = require('express-graphql')
+ */
+const graphqlHTTP = require('express-graphql').graphqlHTTP; 
 
-const app = express();
 const PORT = 4000;
 
+const app = express();
+
+// bind express with graphql
+app.use('/graphql', graphqlHTTP({
+    // pass in a schema property
+    
+}));
+
 app.listen(PORT, () => {
-    console.log(`now listening for requests on port ${PORT}`)
+    console.log(`now listening for requests on port ${PORT}`);
 });
