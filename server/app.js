8b13d9bb446239a93182e70fe2753e043f5d3e58
anthonycheitzeberg <a.heitzeberg007@gmail.com>
2021-09-14 21:47:26 +0800

    add root query and mutation

--- server/app.js
+++ server/app.js
@@ -5,16 +5,31 @@ const express = require('express');
  *  (a)  const graphqlHTTP = require('express-graphql').graphqlHTTP; 
  *  (b)  const { graphqlHTTP } = require('express-graphql')
  */
-const graphqlHTTP = require('express-graphql').graphqlHTTP; 
+const graphqlHTTP = require('express-graphql').graphqlHTTP;
+const schema = require('./schema/schema');
+const mongoose = require('mongoose');
 
+const uri = "mongodb+srv://heitzebergUser:heitzebergUser@gql-tutorial.vdlxe.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 const PORT = 4000;
-
 const app = express();
+
+// connect to db in mongoDB
+try {
+    mongoose.connect(
+        uri,
+        { useNewUrlParser: true, useUnifiedTopology: true },
+        () => console.log("Mongoose is connected")
+    );
+
+} catch (e) {
+    console.log("could not connect");
+}
 
 // bind express with graphql
 app.use('/graphql', graphqlHTTP({
     // pass in a schema property
-    
+    schema,
+    graphiql: true
 }));
 
 app.listen(PORT, () => {
--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,19 +1,103 @@
 const graphql = require('graphql');
+const _ = require('lodash')
+const Book = require('../models/book')
+const Author = require('../models/author')
 
-const { GraphQLObjectType, GraphQLString } = graphql;
+const {
+    GraphQLObjectType,
+    GraphQLSchema,
+    GraphQLList,
+    GraphQLString,
+    GraphQLID,
+    GraphQLInt
+} = graphql;
 
 const BookType = new GraphQLObjectType({
     name: 'Book',
     fields: () => ({
-        id: { type: GraphQLString },
+        id: { type: GraphQLID },
         name: { type: GraphQLString },
-        genre: { type: GraphQLString }
+        genre: { type: GraphQLString },
+        author: {
+            type: AuthorType,
+            resolve(parent, args) {
+                // return _.find(authors, { id: parent.authorId })
+            }
+        }
+    })
+});
+
+const AuthorType = new GraphQLObjectType({
+    name: 'Author',
+    fields: () => ({
+        id: { type: GraphQLID },
+        name: { type: GraphQLString },
+        age: { type: GraphQLInt },
+        books: {
+            type: new GraphQLList(BookType),
+            resolve(parent, args) {
+                // return _.filter(books, { authorId: parent.id })
+            }
+        }
     })
 });
 
 const RootQuery = new GraphQLObjectType({
     name: "RootQueryType",
     fields: {
-        book: {}
+        book: {
+            type: BookType,
+            args: { id: { type: GraphQLID } },
+            resolve(parent, args) {
+                // return _.find(books, { id: args.id })
+            }
+        },
+        author: {
+            type: AuthorType,
+            args: { id: { type: GraphQLID } },
+            resolve(parent, args) {
+                // return _.find(authors, { id: args.id })
+            }
+        },
+        books: {
+            type: new GraphQLList(BookType),
+            resolve(parent, args) {
+                // return books
+            }
+        },
+        authors: {
+            type: new GraphQLList(AuthorType),
+            resolve(parent, args) {
+                // return authors
+            }
+        }
     }
-})
+});
+
+const Mutation = new GraphQLObjectType({
+    name: 'Mutation',
+    fields: {
+        addAuthor: {
+            type: AuthorType,
+            args: {
+                name: { type: GraphQLString },
+                age: { type: GraphQLInt }
+            },
+            resolve(parent, args) {
+                let author = new Author({
+                    name: args.name,
+                    age: args.age
+                });
+                console.log('data added to db')
+                return author.save();
+            }
+        }
+    }
+});
+
+module.exports = new GraphQLSchema({
+    query: RootQuery,
+    mutation: Mutation
+});
+
+
